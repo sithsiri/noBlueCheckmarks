d1f5be59efb7585ee616911b3f59803102e1d982
sithsiri <sith@live.com>
2023-06-02 04:42:50 -0500

    init

---
+++ src/nobluecheckmarks.js
@@ -0,0 +1,59 @@
+if (!window.tweetSetupHasRun) {
+  window.tweetSetupHasRun = true;
+  var styleElement = document.createElement('style');
+  styleElement.textContent = '.hasVerifiedAccount { display: none; }';
+  styleElement.id = "verifiedTweetStyle"
+  document.head.appendChild(styleElement);
+
+  function showVerifiedTweets() {
+    styleElement.textContent = '.hasVerifiedAccount {}';
+  }
+  function hideVerifiedTweets() {
+    styleElement.textContent = '.hasVerifiedAccount { display: none; }';
+  }
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "showVerifiedTweets") {
+      showVerifiedTweets();
+    } else if (message.command === "hideVerifiedTweets") {
+      hideVerifiedTweets();
+    }
+  });
+
+  function observeDivCreation(callback) {
+    var observer = new MutationObserver(function(mutationsList) {
+      mutationsList.forEach(function(mutation) {
+        if (mutation.type === 'childList') {
+          mutation.addedNodes.forEach(function(node) {
+            if (node.nodeType === Node.ELEMENT_NODE && node.matches('div[data-testid="cellInnerDiv"]')) {
+              callback(node);
+            }
+          });
+        }
+      });
+    });
+
+    observer.observe(document, { childList: true, subtree: true });
+  }
+
+  observeDivCreation(function(newDiv) {
+    if (hasVerifiedAccount(newDiv)) {
+      // newDiv.style.display = 'none';
+      newDiv.classList.add('hasVerifiedAccount');
+      console.log("Found tweet by verified user " + getUsername(newDiv));
+    }
+    else {
+      newDiv.classList.add('hasUnverifiedAccount');
+    }
+  });
+
+  function hasVerifiedAccount(element) {
+    var svgElements = element.querySelectorAll('svg[aria-label="Verified account"]');
+    return svgElements.length > 0;
+  }
+
+  function getUsername(element) {
+    var usernameDiv = element.querySelectorAll('div[data-testid="User-Name"]')[0];
+    if (!usernameDiv) return "unknown";
+    return usernameDiv.children[1].children[0].children[0].children[0].children[0].children[0].innerHTML;
+  }
+} else {console.log("noBlueCheckmarks JS was run an extra time")}
---
+++ src/popup/script.js
@@ -0,0 +1,72 @@
+/**
+ * CSS to hide everything on the page,
+ * except for elements that have the "beastify-image" class.
+ */
+const hidePage = `body > :not(.beastify-image) {
+                    display: none;
+                  }`;
+
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+
+    function showTweets(tabs) {
+			browser.tabs.sendMessage(tabs[0].id, {
+				command: "showVerifiedTweets",
+			});
+    }
+
+    function hideTweets(tabs) {
+			browser.tabs.sendMessage(tabs[0].id, {
+				command: "hideVerifiedTweets",
+			});
+    }
+
+    /**
+     * Just log the error to the console.
+     */
+    function reportError(error) {
+      console.error(`Could not modify tweets: ${error}`);
+    }
+
+    /**
+     * Get the active tab,
+     * then call function as appropriate.
+     */
+    if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
+      // Ignore when click is not on a button within <div id="popup-content">.
+      return;
+    }
+    if (e.target.type === "reset") {
+      browser.tabs.query({active: true, currentWindow: true})
+        .then(hideTweets)
+        .catch(reportError);
+    } else {
+      browser.tabs.query({active: true, currentWindow: true})
+        .then(showTweets)
+        .catch(reportError);
+    }
+  });
+}
+
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute noBlueCheckmarks content script: ${error.message}`);
+}
+
+/**
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
+browser.tabs.query({active: true, currentWindow: true})
+  .then(listenForClicks)
+  .catch(reportExecuteScriptError);
